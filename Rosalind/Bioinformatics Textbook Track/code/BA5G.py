12d2b6f518904008a1cb696dc1a379a04629b3b9
delta-life <iljung0810@gmail.com>
2022-07-29 21:45:36 +0900

    modification to the algorithm

--- Rosalind/Bioinformatics Textbook Track/code/BA5G.py
+++ Rosalind/Bioinformatics Textbook Track/code/BA5G.py
@@ -5,18 +5,18 @@ from utilities import get_file, get_answer_file
 
 def edit_distance(str1, str2):
     cost = [[0]*(len(str1)+1) for _ in range(len(str2)+1)]
-    matches = [[1]*(len(str1)) for _ in range(len(str2))]
+
     for i in range(len(cost)):
         cost[i][0] = 1 * i
     for i in range(len(cost[0])):
         cost[0][i] = 1 * i
-    for i in range(len(matches)):
-        for j in range(len(matches[0])):
-            if str1[j] == str2[i]:
-                matches[i][j] = 0
-    for i in range(1, len(str2)+1):
-        for j in range(1, len(str1)+1):
-            cost[i][j] = min(cost[i-1][j]+1, cost[i][j-1]+1, cost[i-1][j-1]+matches[i-1][j-1])
+    for i in range(1, len(cost)):
+        for j in range(1, len(cost[0])):
+            if str1[j-1] == str2[i-1]:
+                cost[i][j] = min(cost[i-1][j]+1, cost[i][j-1]+1, cost[i-1][j-1])
+            else:
+                cost[i][j] = min(cost[i-1][j]+1, cost[i][j-1]+1, cost[i-1][j-1]+1)
+
     return cost[-1][-1]
 
 
